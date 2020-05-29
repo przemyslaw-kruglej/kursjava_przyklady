9e8b21cf8a19a02a31ade212b96827e0f52e26c4
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2020-05-29 18:41:51 +0200

    Finalna wersja przykladu Pracownik.java

--- Rozdzial_10__Dziedzicznie_i_polimorfizm/prostyprzyklad/Pracownik.java
+++ Rozdzial_10__Dziedzicznie_i_polimorfizm/prostyprzyklad/Pracownik.java
@@ -3,18 +3,29 @@ package prostyprzyklad;
 public class Pracownik extends Osoba {
   public int numerIdentyfikatora;
 
+  public String toString() {
+    return "Pracownik " + imie + " " + nazwisko +
+        ", identyfikator: " + numerIdentyfikatora;
+  }
+
   public static void main(String[] args) {
     Osoba pewnaOsoba = new Osoba();
     pewnaOsoba.imie = "Jan";
     pewnaOsoba.nazwisko = "Kowalski";
 
-    System.out.println(pewnaOsoba);
+    System.out.println(pewnaOsoba.toString());
 
     Pracownik pewienPracownik = new Pracownik();
     pewienPracownik.imie = "Joanna";
     pewienPracownik.nazwisko = "Sikorska";
     pewienPracownik.numerIdentyfikatora = 1234;
 
-    System.out.println(pewienPracownik);
+    System.out.println(pewienPracownik.toString());
+
+    Osoba innaOsoba = new Pracownik();
+    innaOsoba.imie = "Adrian";
+    innaOsoba.nazwisko = "Sochacki";
+
+    System.out.println(innaOsoba.toString());
   }
 }
