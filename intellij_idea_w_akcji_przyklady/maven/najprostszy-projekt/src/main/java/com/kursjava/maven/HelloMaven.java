9d9f7a67d189610c552e4630176b259d78f91234
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2020-06-05 16:42:12 +0200

    IntelliJ IDEA w akcji: przykladowy projekt Maven

---
+++ intellij_idea_w_akcji_przyklady/maven/najprostszy-projekt/src/main/java/com/kursjava/maven/HelloMaven.java
@@ -0,0 +1,7 @@
+package com.kursjava.maven;
+
+public class HelloMaven {
+  public static void main(String[] args) {
+    System.out.println("Hello Maven!");
+  }
+}
---
+++ intellij_idea_w_akcji_przyklady/maven/najprostszy-projekt/src/test/java/com/kursjava/maven/HelloMavenTest.java
@@ -0,0 +1,11 @@
+package com.kursjava.maven;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class HelloMavenTest {
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
