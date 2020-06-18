8e1dc68fd6e5c32e0b8afa2418c729894cc3dc03
Przemyslaw Kruglej <przemyslaw.kruglej@gmail.com>
2020-06-18 11:56:49 +0200

    Domyślne konstruktory - dodanie komentarza informującego o błędzie kompilacji

--- Rozdzial_10__Dziedzicznie_i_polimorfizm/domyslneKonstruktory/Monitor.java
+++ Rozdzial_10__Dziedzicznie_i_polimorfizm/domyslneKonstruktory/Monitor.java
@@ -3,6 +3,8 @@ package domyslneKonstruktory;
 public class Monitor extends Produkt {
   private int przekatnaEkranu;
 
+  // blad kompilacji! nie wywolujemy konstruktora
+  // klasy bazowej, a nie posiada on konstruktora bezargumentowego
   public Monitor(int przekatnaEkranu) {
     this.przekatnaEkranu = przekatnaEkranu;
   }
